c1bdee0415e8956178095c24478ff5bd95490947
Amelia Alfonso <alfonsoa@bu.edu>
2024-12-11 22:34:07 -0500

    Fixing workflow

--- test_app.py
+++ test_app.py
@@ -5,5 +5,4 @@ def test_index_page():
     with app.test_client() as client:
         response = client.get('/')
         assert response.status_code == 200
-        # Check that the expected text is present on the page
-        assert b"Select a Bus Route" in response.data
+        assert b'<select name="route_selection">' in response.data
